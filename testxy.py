b888e10fc77d1778a187df19a291ecc8c8dc17e9
chsj9251 <168413101+chsj9251@users.noreply.github.com>
2024-06-14 16:56:42 +0900

    Add files via upload

--- testxy.py
+++ testxy.py
@@ -9,7 +9,7 @@ from geometry_msgs.msg import TransformStamped
 
 db = None
 
-def update_robot_position(x, y, yaw):
+def update_robot_position(x, y, z, w):
     global db
     cursor = db.cursor()
     check_sql = "SELECT * FROM robot WHERE robot_name = %s"
@@ -17,11 +17,11 @@ def update_robot_position(x, y, yaw):
     existing_row = cursor.fetchone()
 
     if existing_row is None:
-        insert_sql = "INSERT INTO robot (robot_name, robot_x, robot_y, robot_yaw, robot_status) VALUES (%s, %s, %s, %s, %s)"
-        cursor.execute(insert_sql, ('RPM-01', x, y, yaw, 'online'))
+        insert_sql = "INSERT INTO robot (robot_name, robot_x, robot_y, robot_z, robot_w, robot_status) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(insert_sql, ('RPM-01', x, y, z, w, 'online'))
     else:
-        update_sql = "UPDATE robot SET robot_x = %s, robot_y = %s, robot_yaw = %s, robot_status = %s WHERE robot_name = %s"
-        cursor.execute(update_sql, (x, y, yaw, 'online', 'RPM-01'))
+        update_sql = "UPDATE robot SET robot_x = %s, robot_y = %s, robot_z = %s, robot_w = %s, robot_status = %s WHERE robot_name = %s"
+        cursor.execute(update_sql, (x, y, z, w, 'online', 'RPM-01'))
 
     db.commit()
     cursor.close()
@@ -39,20 +39,15 @@ def tf_callback(tf_msg):
     trans = tf_msg.transform.translation
     rot = tf_msg.transform.rotation
 
-    # 변환 메시지에서 x, y 좌표와 회전(yaw)을 읽어옴
-    x, y = trans.x, trans.y
+    # 변환 메시지에서 x, y 좌표와 z, w 값을 읽어옴
+    x, y, z, w = trans.x, trans.y, rot.z, rot.w
 
-    # 쿼터니언을 오일러 각도로 변환하여 yaw(회전)을 얻음
-    quaternion = (rot.x, rot.y, rot.z, rot.w)
-    euler = tf.transformations.euler_from_quaternion(quaternion)
-    yaw = euler[2]  # Z 축 방향의 회전 (yaw)을 얻음
-
-    update_robot_position(x, y, yaw)
-    rospy.loginfo("Updated position: x=%f, y=%f, yaw=%f" % (x, y, yaw))
+    update_robot_position(x, y, z, w)
+    rospy.loginfo("Updated position: x=%f, y=%f, z=%f, w=%f" % (x, y, z, w))
 
 def signal_handler(signal, frame):
     global db
-    rospy.loginfo("Node is Close..")
+    rospy.loginfo("Node is shutting down...")
     update_robot_offline()
     rospy.signal_shutdown("Node is Closed")
 
@@ -60,21 +55,29 @@ def main():
     global db
     rospy.init_node('testdb')
 
-    db = pymysql.connect(
-        host="13.124.83.151",
-        user="root",
-        password="1235",
-        database="rpm"
+    try:
+        db = pymysql.connect(
+            host="13.124.83.151",
+            user="root",
+            password="1235",
+            database="rpm"
         )
+        rospy.loginfo("Database connected successfully")
+    except Exception as e:
+        rospy.logerr("Failed to connect to database: %s", e)
+        return
 
     rospy.Subscriber('/tf', TransformStamped, tf_callback)
+    rospy.loginfo("Subscribed to /tf")
 
     rate = rospy.Rate(10.0)
+    rospy.loginfo("Node started")
 
     while not rospy.is_shutdown():
         rate.sleep()
 
     db.close()
+    rospy.loginfo("Database connection closed")
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)  # Ctrl+C 종료 시 시그널 핸들러 등록
