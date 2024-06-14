72a6e25829681eb6c8997ca636878ebf7b3754c4
chsj9251 <168413101+chsj9251@users.noreply.github.com>
2024-06-14 13:19:35 +0900

    Add files via upload

--- testxy.py
+++ testxy.py
@@ -5,22 +5,23 @@ import rospy
 import tf
 import pymysql
 import signal
+from geometry_msgs.msg import TransformStamped
 
 db = None
 
-def update_robot_position(x, y):
+def update_robot_position(x, y, yaw):
     global db
     cursor = db.cursor()
-    check_sql = "SELECT * FROM robot WHERE name = %s"
+    check_sql = "SELECT * FROM robot WHERE robot_name = %s"
     cursor.execute(check_sql, ('RPM-01',))
     existing_row = cursor.fetchone()
 
     if existing_row is None:
-        insert_sql = "INSERT INTO robot (name, current_x, current_y, status) VALUES (%s, %s, %s, %s)"
-        cursor.execute(insert_sql, ('RPM-01', x, y, 'online'))
+        insert_sql = "INSERT INTO robot (robot_name, robot_x, robor_y, robot_r, robot_status) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_sql, ('RPM-01', x, y, yaw, 'online'))
     else:
-        update_sql = "UPDATE robot SET current_x = %s, current_y = %s, status = %s WHERE name = %s"
-        cursor.execute(update_sql, (x, y, 'online', 'RPM-01'))
+        update_sql = "UPDATE robot SET robot_x = %s, robot_x = %s, robot_r = %s, robot_status = %s WHERE robot_name = %s"
+        cursor.execute(update_sql, (x, y, yaw, 'online', 'RPM-01'))
 
     db.commit()
     cursor.close()
@@ -28,10 +29,25 @@ def update_robot_position(x, y):
 def update_robot_offline():
     global db
     cursor = db.cursor()
-    update_sql = "UPDATE robot SET status = 'offline' WHERE name = 'RPM-01'"
+    update_sql = "UPDATE robot SET robot_status = 'offline' WHERE robot_name = 'RPM-01'"
     cursor.execute(update_sql)
     db.commit()
     cursor.close()
+
+def tf_callback(tf_msg):
+    trans = tf_msg.transform.translation
+    rot = tf_msg.transform.rotation
+
+    # 변환 메시지에서 x, y 좌표와 회전(yaw)을 읽어옴
+    x, y = trans.x, trans.y
+
+    # 쿼터니언을 오일러 각도로 변환하여 yaw(회전)을 얻음
+    quaternion = (rot.x, rot.y, rot.z, rot.w)
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+    yaw = euler[2]  # Z 축 방향의 회전 (yaw)을 얻음
+
+    update_robot_position(x, y, yaw)
+    rospy.loginfo("Updated position: x=%f, y=%f, yaw=%f" % (x, y, yaw))
 
 def signal_handler(signal, frame):
     global db
@@ -44,26 +60,17 @@ def main():
     rospy.init_node('testdb')
 
     db = pymysql.connect(
-        host="192.168.0.118",
-        user="rpm",
-        password="11223344",
+        host="13.124.83.151",
+        user="root",
+        password="1235",
         database="rpm"
         )
 
-    listener = tf.TransformListener()
+    rospy.Subscriber('/tf', TransformStamped, tf_callback)
 
     rate = rospy.Rate(10.0)
 
     while not rospy.is_shutdown():
-        try:
-            (trans, rot) = listener.lookupTransform('/map', '/base_footprint', rospy.Time(0))
-            x, y = trans[0], trans[1]
-            update_robot_position(x, y)
-            rospy.loginfo("Updated position: x=%f, y=%f" % (x, y))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            rospy.logwarn("TF lookup failed")
-            continue
-
         rate.sleep()
 
     db.close()
