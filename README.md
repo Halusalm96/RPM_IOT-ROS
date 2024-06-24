07c46ad80b1039d6b48c59c844b7a1779b2ea662
user1 <chlwlsdnd72@gmail.com>
2024-06-24 12:06:32 +0900

    2024/06/24_12:00_nodelet

---
+++ README.md
@@ -0,0 +1,6 @@
+2024/5/29 urdf변형한 것 urdf 디렉터리에 존재 
+
+nodelet 의존성 패키지
+sudo apt-get install ros-melodic-usb-cam
+sudo apt-get install pkg-config
+sudo apt-get install libusb-1.0-0-dev
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/user1/catkin_ws/src/nodelet_hello_world
+# Build directory: /home/user1/catkin_ws/src/nodelet_hello_world/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
